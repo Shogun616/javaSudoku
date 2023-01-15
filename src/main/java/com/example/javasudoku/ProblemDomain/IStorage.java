9382a9237423fc68700db9d11875b01731e20571
Philip Mattsson <philipm606@gmail.com>
2023-01-15 16:57:47 +0100

    Added IStorage Interface

---
+++ src/main/java/com/example/javasudoku/ProblemDomain/IStorage.java
@@ -0,0 +1,8 @@
+package com.example.javasudoku.ProblemDomain;
+
+import java.io.IOException;
+
+public interface IStorage {
+    void updateGameData(SudokuGame game) throws IOException;
+    SudokuGame getGameData() throws IOException;
+}
